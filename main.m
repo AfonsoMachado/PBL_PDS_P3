869ed275b4034392961b37f839475d6f67255614
Afonso Machado <afonsosmachado@gmail.com>
2022-11-03 17:16:14 -0300

    main file

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 % Frequencia de corte
-fc = 1750;
+fc1 = 1750;
+fc2 = 2500;
 % Frequencia de amostragem
 fs = 8000;
 % Largura da banda de transição
@@ -9,7 +10,8 @@ M = 52;
 
 % Obtendo as frequencia discretas, da banda de passagem, banda de rejeição
 % e de corte
-[discrete_passband, discrete_stopband, discrete_cutoff] = discrete_frequencies(fc, bandwidth, fs);
+[~, ~, discrete_cutoff1] = discrete_frequencies(fc1, bandwidth, fs);
+[~, ~, discrete_cutoff2] = discrete_frequencies(fc2, bandwidth, fs);
 
-hd = high_pass_filter(fc,discrete_cutoff, M, fs);
+hd = band_stop_filter(fc1, discrete_cutoff1, fc2, discrete_cutoff2, M, fs);
 stem(hd);
