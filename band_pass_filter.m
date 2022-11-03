565abfc7e5b35cd88205b4fdbee4208690b2b313
Afonso Machado <afonsosmachado@gmail.com>
2022-11-03 17:15:51 -0300

    band pass filter

---
+++ band_pass_filter.m
@@ -0,0 +1,15 @@
+% Filtro passa faixa
+function hd = band_pass_filter(fc1, wc1, fc2, wc2, M, fs)
+    % para n[0]
+    hd0 = 2 * ((fc2 - fc1) / fs);
+
+    % Apenas a parte positiva, a outra parte resulta de um flip esta.
+    n = 1:M / 2;
+
+    % para n[x], com x variando entre 1 e M/2
+    hd1 = basic_filter(fc1, wc1, n, fs);
+    hd2 = basic_filter(fc2, wc2, n, fs);
+    hd3 = hd2 - hd1;
+
+    hd = [flip(hd3), hd0, hd3];
+end
